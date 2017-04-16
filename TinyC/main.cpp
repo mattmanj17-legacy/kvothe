bd39d7c7a71f0841e0cdec9ba4f5294f2babe30e
Matthew Draper <draper_matthewjohn@yahoo.com>
2017-04-15 17:59:37 -0700

    start tinyc project

---
+++ TinyC/main.cpp
@@ -0,0 +1,70 @@
+// tiny c compiler and virtual machine
+
+/*
+	program
+	   : statement +
+	   ;
+
+	statement
+	   : 'if' paren_expr statement
+	   | 'if' paren_expr statement 'else' statement
+	   | 'while' paren_expr statement
+	   | 'do' statement 'while' paren_expr ';'
+	   | '{' statement* '}'
+	   | expr ';'
+	   | ';'
+	   ;
+
+	paren_expr
+	   : '(' expr ')'
+	   ;
+
+	expr
+	   : test
+	   | id '=' expr
+	   ;
+
+	test
+	   : sum
+	   | sum '<' sum
+	   ;
+
+	sum
+	   : term
+	   | sum '+' term
+	   | sum '-' term
+	   ;
+
+	term
+	   : id
+	   | integer
+	   | paren_expr
+	   ;
+
+	id
+	   : STRING
+	   ;
+
+	integer
+	   : INT
+	   ;
+
+
+	STRING
+	   : [a-z]+
+	   ;
+
+
+	INT
+	   : [0-9] +
+	   ;
+
+	WS
+	   : [ \r\n\t] -> skip
+	   ;
+*/
+
+int main()
+{
+
+}
