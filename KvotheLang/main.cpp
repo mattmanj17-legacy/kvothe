04c7d2ede12f4387cbad7ba4cd64d29d0cb5a71b
Matthew Draper <draper_matthewjohn@yahoo.com>
2017-04-22 10:44:21 -0700

    still in cleanup, committing project rename

---
+++ KvotheLang/main.cpp
@@ -0,0 +1,265 @@
+// tiny c compiler and virtual machine
+
+#include <string>
+
+#include "ary.h"
+
+using std::string;
+
+// TOKen Kind
+
+enum TOKK
+{
+	TOKK_NIL,
+	TOKK_EOI,
+	TOKK_INT,
+	TOKK_ID,
+	TOKK_PLUS,
+	TOKK_SUB,
+	TOKK_LT,
+	TOKK_LPAREN,
+	TOKK_RPAREN,
+	TOKK_EQUALS,
+	TOKK_LBRACE,
+	TOKK_RBRACE,
+	TOKK_DO,
+	TOKK_WHILE,
+	TOKK_IF,
+	TOKK_ELSE,
+	TOKK_SEMICOLON,
+};
+
+// RULE Kind
+
+enum RULEK
+{
+	RULEK_NIL,
+	RULEK_Atom,
+	RULEK_Sum,
+	RULEK_TestExpr,
+	RULEK_ParenExpr,
+	RULEK_Statement,
+	RULEK_Program,
+};
+
+// a node in the parse tree
+
+class CParseNode // tag = node
+{
+	friend class CParseTree;
+
+public:
+
+	void			AddChild(int iNode);
+
+	RULEK			Rulek() const;
+	TOKK			Tokk() const;
+
+	CParseNode		NodeChild(int iChild) const;
+	int				CChild() const;
+
+	string			Str() const;
+
+private:
+					CParseNode();
+
+	RULEK			m_rulek;
+	TOKK			m_tokk;
+	string			m_str;
+
+	CDynAry<int>	m_daryINodeChild;
+};
+
+// the actual parse tree (mostly just a place to store all of the nodes)
+
+class CParseTree // tag = parsetree
+{
+public:
+	int	INodeCreate(RULEK rulek);
+	int	INodeCreate(TOKK tokk);
+	int	INodeCreate(TOKK tokk, string str);
+
+	CParseNode * PNodeFromINode(int iNode);
+
+private:
+	CDynAry<CParseNode> m_daryNode;
+};
+
+// tokenizer
+
+class CTokenizer // tag = tokenizer
+{
+public:
+					CTokenizer(CParseTree * pParsetree);
+
+	int				INodeNext();
+
+	void			SetInput(string str);
+
+private:
+	int				INodeKeywordFromString(string strToken);
+
+	string			m_strInput;
+	int				m_iChrCur;
+
+	CParseTree *	m_pParsetree;
+};
+
+// parser
+
+class CParser // tag = parser
+{
+public:
+	int		INodeParseProgram();
+	int		INodeParseStatment();
+	int		INodeParseParenExpr();
+	int		INodeParseTestExpr();
+	int		INodeParseSum();
+	int		INodeParseAtom();
+
+	void	SetInput(string strInput);
+
+private:
+
+	TOKK	TokkCur();
+
+	int		INodeMatch(TOKK tokk);
+};
+
+// BINary OPeration Kind
+
+enum BINOPK
+{
+	BINOPK_NIL,
+	BINOPK_LT,
+	BINOPK_PLUS,
+	BINOPK_SUB
+};
+
+// EXPRession Kind
+
+enum EXPRK
+{
+	EXPRK_Nil,
+	EXPRK_Binop,
+	EXPRK_Int,
+	EXPRK_Id
+};
+
+// AST expression
+
+struct SExpression // tag = expr
+{
+	EXPRK m_exprk;
+
+	union
+	{
+		// EXPRK_Binop
+
+		struct
+		{
+			BINOPK m_binopk;
+			int m_iExprLeft;
+			int m_iExprRight;
+		};
+
+		// EXPRK_Int
+
+		int m_n;
+
+		// EXPRK_Id
+
+		int m_iGlobal;
+	};
+};
+
+// STATement Kind
+
+enum STATK
+{
+	STATK_Nil,
+	STATK_Statements,
+	STATK_If,
+	STATK_While,
+	STATK_DoWhile,
+	STATK_Assign,
+};
+
+// AST Statement
+
+struct SStatement // tag = stat
+{
+	STATK				m_statk;
+
+	union
+	{
+		// STATK_Statements
+		
+		CDynAry<int>	m_aryIStat;
+
+		// STATK_If
+		
+		struct 
+		{
+			int			m_iExprCondition;
+			int			m_iStatBody;
+			bool		m_fHasElse;
+			int			m_iStatElse;
+		};
+
+		// STATK_While / STATK_DoWhile
+
+		struct
+		{
+			int			m_iExprCondition;
+			int			m_iStatBody;
+		};
+
+		// STATK_Assign
+
+		struct
+		{
+			int			m_iExprId; // must be EXPRK_Id
+			int			m_iExprValue;
+		};
+	};
+};
+
+// The Ast
+
+class CAst // tag = ast
+{
+public:
+
+	int						IStatCreate(const CParseNode & pnode);
+	int						IExprCreate(const CParseNode & pnode);
+
+private:
+
+	BINOPK					BinopkFromTokk(TOKK tokk);
+
+	CDynAry<SExpression>	m_daryPExpr;
+	CDynAry<SStatement>		m_daryPStat;
+};
+
+// virtual machine OPCODE
+
+enum OPCODE : int
+{
+	OPCODE_IFETCH,
+	OPCODE_ISTORE,
+	OPCODE_IPUSH,
+	OPCODE_IPOP,
+	OPCODE_IADD,
+	OPCODE_ISUB,
+	OPCODE_ILT,
+	OPCODE_JZ,
+	OPCODE_JNZ,
+	OPCODE_JMP,
+	OPCODE_HALT
+};
+
+int main()
+{
+
+}
